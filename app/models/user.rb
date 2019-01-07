4e5a6ebcddc598d911c4e677d222e418b0e1fa89
alexandersazonof <43858546+alexandersazonof@users.noreply.github.com>
2019-01-07 19:52:46 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   attr_accessor :login
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9]*\z/}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable,
