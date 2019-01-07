a35052d9f1cda146006fe04d2bf2fa7694e49612
alexandersazonof <43858546+alexandersazonof@users.noreply.github.com>
2019-01-07 19:36:22 +0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @popular_tags = ActsAsTaggableOn::Tag.most_used(6)
     if params[:tag].present? 
-      @posts = Post.tagged_with(params[:tag]).paginate(:page => params[:page], per_page: '5')
+      @posts = Post.tagged_with(params[:tag]).paginate(:page => params[:page], per_page: 5)
     else
       @posts = Post.paginate(:page => params[:page], per_page: '5')
     end
