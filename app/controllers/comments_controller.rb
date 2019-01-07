e4cc7373f6ebc28196ecdff69fb5bd3b0736bf2e
Alexander <alexandersazonof0@gmail.com>
2019-01-07 21:52:18 +0300

    fix busg

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @user_id = current_user
+    @user_id = current_user.id
     body = comment_params[:body]
     @comment = @post.comments.create(body: body, user_id: @user_id)
     redirect_to post_path(@post)
