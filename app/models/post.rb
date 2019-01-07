c99e4bdd18b67ac07351f4f528c43119cedd65c5
alexandersazonof <43858546+alexandersazonof@users.noreply.github.com>
2019-01-07 19:02:45 +0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,8 @@
 class Post < ApplicationRecord
-
   validates :content, presence: true
   validates :user_id, presence: true
-
-  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable 
   acts_as_taggable_on :tags
-
   default_scope -> { order('created_at DESC') }
-
   belongs_to :user
 end
